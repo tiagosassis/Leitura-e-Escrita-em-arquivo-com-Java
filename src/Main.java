92932c832ed3846aff98ed7a814d5efb92a8dfc6
Tiago Silva de Assis <109762223+tiagosassis@users.noreply.github.com>
2022-08-05 14:24:24 -0300

    Alterações no Main
    usando métodos no static de Arquivo no Main sem instanciar objeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String texto = Arquivo.Reader("C:\\Users\\tiagu\\Documents\\Arquivo.txt");
+        JOptionPane.showMessageDialog(null, texto);
+        Arquivo.Writer("C:\\Users\\tiagu\\Documents\\Arquivo.txt", "Hello World");
+        JOptionPane.showMessageDialog(null, Arquivo.Reader("C:\\Users\\tiagu\\Documents\\Arquivo.txt"));
     }
 }
